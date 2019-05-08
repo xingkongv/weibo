a2e9a288d18955b9cbaee5f72386a6b3eb5e38c5
cw <xingcengv@gamil.com>
2019-05-08 16:59:06 +0800

    命名路由

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get( '/' , 'StaticPagesController@home' );
-Route::get( '/help' , 'StaticPagesController@help' );
-Route::get( '/about' , 'StaticPagesController@about' );
+Route::get( '/' , 'StaticPagesController@home' )->name('home');
+Route::get( 'help' , 'StaticPagesController@help' )->name('help');
+Route::get( 'about' , 'StaticPagesController@about' )->name('about');
