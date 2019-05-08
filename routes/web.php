79b7d79072dd0a55f4a04b9f7a3685dc7497771f
cw <xingcengv@gamil.com>
2019-05-08 17:03:39 +0800

    用户注册页面

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,5 @@ Route::get('/', function () {
 Route::get( '/' , 'StaticPagesController@home' )->name('home');
 Route::get( 'help' , 'StaticPagesController@help' )->name('help');
 Route::get( 'about' , 'StaticPagesController@about' )->name('about');
+
+Route::get('signup', 'UsersController@create')->name('signup');
