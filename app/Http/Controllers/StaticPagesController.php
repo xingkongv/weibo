a758a14ac5b9bf5b5d2c2fc12b149a6a35be5f84
vagrant <vagrant@homestead>
2019-05-08 03:52:01 +0000

    基础页面

---
+++ app/Http/Controllers/StaticPagesController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class StaticPagesController extends Controller
+{
+
+    public function home()
+    {
+        return view( 'static_page/home' );
+    }
+
+    public function help()
+    {
+        return view( 'static_page/help' );
+    }
+
+    public function about()
+    {
+        return view( 'static_page/about' );
+    }
+}
