4d320ef9db4ed11acdf9fb90a0758bccdf5fdbaa
cw <xingcengv@gamil.com>
2019-05-09 15:04:33 +0800

    增加 CSRF 验证

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -17,4 +17,14 @@ class UsersController extends Controller
         $user->gravatar('140');
         return view('users.show', compact('user'));
     }
+
+    public function store(Request $request)
+    {
+        $this->validate( $request , [
+            'name' => 'required|max:50',
+            'email' => 'required|email|uniqe:users|max:255',
+            'password' => 'required|confirmed|min:6',
+        ] );
+        return;
+    }
 }
