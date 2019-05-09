a800449aef0a0ed21691b33f2b8735f24e5cacbe
cw <xingcengv@gamil.com>
2019-05-09 18:52:22 +0800

    登录时记住我

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -14,22 +14,18 @@ class SessionsController extends Controller
 
     public function store(Request $request)
     {
-        $credentials = $request->validate([
-            'email' => 'required|email|max:255',
-            'password' => 'required'    
-        ]);
+       $credentials = $this->validate($request, [
+           'email' => 'required|email|max:255',
+           'password' => 'required'
+       ]);
 
-        if (Auth::attempt( $credentials )) {
-            // 登录成功后的相关操作
-            session()->flash( 'success' , '欢迎回来' );
-            return redirect()->route( 'users.show' , [Auth::user()] );
-        } else {
-            // 登录失败后的相关操作
-            session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
-            return redirect()->back()->withInput();
-        }
- 
-        return;
+       if (Auth::attempt($credentials, $request->has('remember'))) {
+           session()->flash('success', '欢迎回来！');
+           return redirect()->route('users.show', [Auth::user()]);
+       } else {
+           session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
+           return redirect()->back()->withInput();
+       }
     }
 
     
