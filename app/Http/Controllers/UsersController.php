4671c312a40bbbfb4284ab473792823edf18b5c8
cw <xingcengv@gamil.com>
2019-05-09 10:55:34 +0800

    用户显示页面

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\User;
 
 use Illuminate\Http\Request;
 
@@ -9,5 +10,11 @@ class UsersController extends Controller
     public function create()
     {
         return view('users.create');
+    }
+
+    public function show(User $user)
+    {
+        $user->gravatar('140');
+        return view('users.show', compact('user'));
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -38,4 +38,18 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * 为 gravatar 方法传递的参数 size 指定了默认值 100；
+     * 通过 $this->attributes['email'] 获取到用户的邮箱；
+     * 使用 trim 方法剔除邮箱的前后空白内容；
+     * 用 strtolower 方法将邮箱转换为小写；
+     * 将小写的邮箱使用 md5 方法进行转码；
+     * 将转码后的邮箱与链接、尺寸拼接成完整的 URL 并返回；
+     */
+    public function gravatar( $size = '100' )
+    {
+        $hash = md5( strtolower( trim( $this->attributes['email'] ) ) );
+        return "http://www.gravatar.com/avatar/$hash?s=$size";
+    }
 }
