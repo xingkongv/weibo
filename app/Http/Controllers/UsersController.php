23d606270b023f6545fbd169fc6b09df3ee9dde8
cw <xingcengv@gamil.com>
2019-05-09 15:51:21 +0800

    中文的错误提示信息

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 use App\Models\User;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Validation\Validator;
+use Illuminate\Routing\Controller;
 
 class UsersController extends Controller
 {
@@ -20,11 +23,17 @@ class UsersController extends Controller
 
     public function store(Request $request)
     {
-        $this->validate( $request , [
+        // $this->validate( $request , [
+        //     'name' => 'required|max:50',
+        //     'email' => 'required|email|uniqe:users|max:255',
+        //     'password' => 'required|confirmed|min:6',
+        // ] );
+
+        $request->validate([
             'name' => 'required|max:50',
-            'email' => 'required|email|uniqe:users|max:255',
+            'email' => 'required|email|max:255',
             'password' => 'required|confirmed|min:6',
-        ] );
+        ]);
         return;
     }
 }
