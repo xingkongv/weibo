9aef6321b7ae781465f73bd4da69c70335d2c05a
cw <xingcengv@gamil.com>
2019-05-09 19:29:50 +0800

    更改用户资料

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -39,4 +39,31 @@ class UsersController extends Controller
         session()->flash( 'success' , '欢迎,您将在这里开启一段新旅程' );
         return redirect()->route('users.show' , [$user]);
     }
+
+    public function edit(User $user)
+    {
+        return view('users.edit', compact('user'));
+    }
+
+    public function update(User $user, Request $request)
+    {
+
+        $request->validate([
+            'name' => 'required|max:50',
+            'password' => 'nullable|confirmed|min:6'
+        ]);
+
+        $data = [];
+        $data[ 'name' ] = $request->name;
+        if( $request->password )
+        {
+            $data[ 'password' ] = bcrypt( $request->password );
+        }
+
+        $user->update( $data );
+
+        session()->flash( 'success' , '个人资料更新成功' );
+
+        return redirect()->route('users.show', $user);
+    }
 }
