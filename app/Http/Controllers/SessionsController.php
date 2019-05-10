c81fbc2b6a9e8608214169cf82757de4aabf8274
cw <xingcengv@gamil.com>
2019-05-10 17:25:13 +0800

    用户激活

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -27,11 +27,19 @@ class SessionsController extends Controller
            'password' => 'required'
        ]);
 
-        if (Auth::attempt($credentials, $request->has('remember'))) {
-            session()->flash('success', '欢迎回来！');
-            $fallback = route('users.show', Auth::user());
-            return redirect()->intended($fallback);
-        } else {
+        if (Auth::attempt($credentials, $request->has('remember'))) 
+        {
+            if(Auth::user()->activated) {
+                session()->flash('success', '欢迎回来！');
+                $fallback = route('users.show', Auth::user());
+                return redirect()->intended($fallback);
+            } else {
+                Auth::logout();
+                session()->flash('warning', '你的账号未激活，请检查邮箱中的注册邮件进行激活。');
+                return redirect('/');
+            }
+        } else 
+        {
             session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
             return redirect()->back()->withInput();
         }
