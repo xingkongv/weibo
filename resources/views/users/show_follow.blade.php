fabef63784583ea9948a82158982d085205569c6
cw <xingcengv@gamil.com>
2019-06-10 16:42:48 +0800

    small change

--- resources/views/users/show_follow.blade.php
+++ resources/views/users/show_follow.blade.php
@@ -1,24 +1,35 @@
 @extends('layouts.default')
-@section('title', $title)
+@section('title', $user->name)
 
 @section('content')
-<div class="offset-md-2 col-md-8">
-  <h2 class="mb-4 text-center">{{ $title }}</h2>
+<div class="row">
+  <div class="offset-md-2 col-md-8">
+    <section class="user_info">
+      @include('shared._user_info', ['user' => $user])
+    </section>
 
-  <div class="list-group list-group-flush">
-    @foreach ($users as $user)
-      <div class="list-group-item">
-        <img class="mr-3" src="{{ $user->gravatar() }}" alt="{{ $user->name }}" width=32>
-        <a href="{{ route('users.show', $user) }}">
-          {{ $user->name }}
-        </a>
-      </div>
+    @if (Auth::check())
+      @include('users._follow_form')
+    @endif
 
-    @endforeach
-  </div>
-
-  <div class="mt-3">
-    {!! $users->render() !!}
+    <section class="stats mt-2">
+      @include('shared._stats', ['user' => $user])
+    </section>
+    <hr>
+    <section class="status">
+      @if ($statuses->count() > 0)
+        <ul class="list-unstyled">
+          @foreach ($statuses as $status)
+            @include('statuses._status')
+          @endforeach
+        </ul>
+        <div class="mt-5">
+          {!! $statuses->render() !!}
+        </div>
+      @else
+        <p>没有数据！</p>
+      @endif
+    </section>
   </div>
 </div>
 @stop
